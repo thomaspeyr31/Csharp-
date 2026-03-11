c2308c2be88f09a49260f0af807efa6489a0bd34
thomas peyrusaubes <thomas.peyrusaubes@ynov.com>
2026-03-12 00:10:56 +0100

    Configure Program.cs with Swagger and SQLite DbContext

--- Program.cs
+++ Program.cs
@@ -3,19 +3,31 @@ using TaskBoard.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Ajouter les services au container
+
 builder.Services.AddControllers();
+
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Base de données SQLite
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=taskboard.db"));
 
 var app = builder.Build();
+
+// Configuration du pipeline HTTP
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
